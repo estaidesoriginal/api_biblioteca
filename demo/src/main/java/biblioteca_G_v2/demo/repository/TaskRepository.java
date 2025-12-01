9bcde7da314f5d619d737f3a9ab1f03c09cec36e
ariel <ar.benlulu@duocuc.cl>
2025-12-01 20:59:18 -0300

    Delete demo directory

--- demo/src/main/java/biblioteca_G_v2/demo/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package biblioteca_G_v2.demo.repository;
-
-import biblioteca_G_v2.demo.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
