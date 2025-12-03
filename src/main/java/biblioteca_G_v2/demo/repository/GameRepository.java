ed23567ce989ed7caba05aaf11f3b11571f2a96d
ariel <ar.benlulu@duocuc.cl>
2025-12-03 00:57:32 -0300

    Delete src/main/java/biblioteca_G_v2/demo/repository directory

--- src/main/java/biblioteca_G_v2/demo/repository/GameRepository.java
+++
@@ -1,9 +0,0 @@
-package biblioteca_G_v2.demo.repository;
-
-import biblioteca_G_v2.demo.model.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GameRepository extends JpaRepository<Game, String> {
-}
