168cad19965fd6a5bcac355db1019ff3de08dd0d
ariel <ar.benlulu@duocuc.cl>
2025-12-13 15:44:28 -0300

    Create Roles class with user role constants

---
+++ src/main/java/biblioteca_G_v2/demo/model/Roles.java
@@ -0,0 +1,8 @@
+package biblioteca_G_v2.demo.model;
+
+public class Roles {
+    public static final String USER = "USER";       // Usuario normal (Cliente)
+    public static final String ADMIN = "ADMIN";     // Super administrador
+    public static final String SELLER = "SELLER";   // Vendedor (Gestiona sus productos)
+    public static final String MANAGER = "MANAGER"; // Gerente (Supervisa ventas/usuarios)
+}
