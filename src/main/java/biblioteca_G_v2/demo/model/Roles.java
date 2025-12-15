e34e69efcf49139851f5143072dc3e67c9842610
ariel <ar.benlulu@duocuc.cl>
2025-12-15 14:46:46 -0300

    Update Roles.java

--- src/main/java/biblioteca_G_v2/demo/model/Roles.java
+++ src/main/java/biblioteca_G_v2/demo/model/Roles.java
@@ -1,8 +1,8 @@
 package biblioteca_G_v2.demo.model;
 
 public class Roles {
-    public static final String USER = "USER";       // Usuario normal (Cliente)
-    public static final String ADMIN = "ADMIN";     // Super administrador
-    public static final String SELLER = "SELLER";   // Vendedor (Gestiona sus productos)
-    public static final String MANAGER = "MANAGER"; // Gerente (Supervisa ventas/usuarios)
+    public static final String USER = "USER";  
+    public static final String ADMIN = "ADMIN";   
+    public static final String SELLER = "SELLER";  
+    public static final String MANAGER = "MANAGER";
 }
